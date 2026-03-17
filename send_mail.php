ddc47df2dae2e3cf804b2d6989017053a65de2fd
Hadi <167310042+graafschapcollegestudent@users.noreply.github.com>
2026-03-17 09:32:39 +0100

    test

--- send_mail.php
+++ send_mail.php
@@ -1,21 +1,25 @@
 <?php
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // gegevens ophalen uit formulier
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
 // Email settings
-$to = "jouwstudentmail@graafschapcollege.nl"; // jouw GC mail
+$to = "hadi.idle@student.graafschapcollege.nl"; // jouw GC mail
 $subject = "New Contact Form Submission";
 $body = "Name: $name\nEmail: $email\nMessage: $message";
-$headers = "From: $email";
+$headers = "From: noreply@student.gc-webhosting.nl";
+$headers += "\r\nReply-To: $email";
 
 // Send email
 if (mail($to, $subject, $body, $headers)) {
     echo "Email sent successfully!";
 } else {
     echo "Failed to send email.";
+}
+} else{
+    echo "Invalid request method.";
 }
 
 ?>
